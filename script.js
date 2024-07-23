2337a8da562426eeaa1329bdd2d15b5556d314d7
QHTrung <qhtrung23072000@gmail.com>
2024-07-23 23:25:25 +0700

    feat: switching the active player

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Select elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const curScore0El = document.getElementById('current--0');
@@ -12,7 +14,9 @@ const holdBtnEl = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let scores = [0, 0];
 let curScore = 0;
+let activePlayer = 0;
 
 rollBtnEl.addEventListener('click', () => {
   // 1. Generate random dice roll number
@@ -23,7 +27,12 @@ rollBtnEl.addEventListener('click', () => {
   // 3. if it is 1 switch player turn, else add dice number to the current score
   if (dice !== 1) {
     curScore += dice;
-    curScore0El.textContent = curScore;
+    document.getElementById(`current--${activePlayer}`).textContent = curScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    curScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
